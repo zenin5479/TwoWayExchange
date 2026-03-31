31d678553d6b28f45caf93245d9dbf4b58d9458a
zenin5479 <zenin5479@gmail.com>
2026-03-31 14:20:15 +0300

    Update 4.3 Обновлен метод. Проведен рефакторинг

--- SyncServer/MainForm.cs
+++ SyncServer/MainForm.cs
@@ -37,7 +37,7 @@ namespace SyncServer
                      break;
                   }
 
-                  Log($"Получено: {command}");
+                  Log(string.Format("Получено: {0}", command));
 
                   if (command.Equals("exit", StringComparison.OrdinalIgnoreCase))
                   {
