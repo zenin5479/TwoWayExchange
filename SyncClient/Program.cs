21c7906caab99d6ba34f5df23a8e50eb2a4e4078
zenin5479 <zenin5479@gmail.com>
2026-04-01 10:34:54 +0300

    Update 6.5 Обновлен метод. Проведен рефакторинг

--- SyncClient/Program.cs
+++ SyncClient/Program.cs
@@ -20,14 +20,15 @@ namespace SyncClient
             {
                writer.AutoFlush = true;
                int counter = 0;
-               while (counter <= 1000)
+               while (counter < 100)
                {
                   // Имитация длительных вычислений
                   Thread.Sleep(100); // можно заменить на реальные расчёты
-                  double result = Math.Sqrt(counter++ * 1000);
+                  double result = Math.Sqrt(counter * 1000);
                   // Отправляем результат в stdout
                   Console.WriteLine("Результат: {0:F2}", result);
                   writer.WriteLine(result);
+                  counter++;
                }
             }
          }
