11ce51776db28b19f4d3c34dc15ab5bb50d38897
zenin5479 <zenin5479@gmail.com>
2026-04-01 11:56:40 +0300

    Update 7.8 Обновлен метод. Проведен рефакторинг

--- SyncClient/Program.cs
+++ SyncClient/Program.cs
@@ -14,8 +14,8 @@ namespace SyncClient
          {
             try
             {
-               // Подключаемся к серверу (таймаут 5 секунд)
-               client.Connect(5000);
+               // Подключаемся к серверу (таймаут 1 секунда)
+               client.Connect(1000);
                client.ReadMode = PipeTransmissionMode.Message;
                Console.WriteLine("Подключено к серверу.");
 
@@ -23,17 +23,17 @@ namespace SyncClient
                string message = "Привет от консольного клиента!";
                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
                client.Write(messageBytes, 0, messageBytes.Length);
-               Console.WriteLine(string.Format("Отправлено: {0}", message));
+               Console.WriteLine("Отправлено: {0}", message);
 
                // Читаем ответ
                byte[] buffer = new byte[1024];
                int bytesRead = client.Read(buffer, 0, buffer.Length);
                string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-               Console.WriteLine(string.Format("Получен ответ: {0}", response));
+               Console.WriteLine("Получен ответ: {0}", response);
             }
             catch (Exception ex)
             {
-               Console.WriteLine(string.Format("Ошибка: {0}", ex.Message));
+               Console.WriteLine("Ошибка: {0}", ex.Message);
             }
          }
 
