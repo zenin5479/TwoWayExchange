0522509f36f2f0c2d99d2cabe8f35af8805ed95d
zenin5479 <zenin5479@gmail.com>
2026-04-02 13:34:15 +0300

    Update 12.2 Обновлен метод. Проведен рефакторинг

--- SyncClient/Program.cs
+++ SyncClient/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SyncClient
+﻿using System.IO.Pipes;
+using System.IO;
+using System;
+
+namespace SyncClient
 {
    internal class Program
    {
       static void Main()
       {
+         // Имя канала должно совпадать с именем, используемым в WinForms
+         using (var client = new NamedPipeClientStream(".", "twoWayPipe", PipeDirection.InOut))
+         {
+            Console.WriteLine("Клиент: подключение к каналу...");
+            client.Connect(); // Синхронное подключение
 
-      }
+            // Чтение сообщения от WinForms
+            var reader = new StreamReader(client);
+            var writer = new StreamWriter(client) { AutoFlush = true };
+
+            string received = reader.ReadLine();
+            Console.WriteLine($"Клиент получил: {received}");
+
+            // Формирование ответа
+            string response = $"Ответ на '{received}'";
+            writer.WriteLine(response);
+            Console.WriteLine($"Клиент отправил: {response}");
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+         }
    }
 }
