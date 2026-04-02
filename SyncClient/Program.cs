215f66aa5e6a8a567c7661269bcb9f55247ead54
zenin5479 <zenin5479@gmail.com>
2026-04-02 12:05:50 +0300

    Update 11.4 Обновлен метод. Проведен рефакторинг

--- SyncClient/Program.cs
+++ SyncClient/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.IO;
-using System.IO.Pipes;
-using System.Text;
-
-namespace SyncClient
+﻿namespace SyncClient
 {
    internal class Program
    {
       static void Main()
       {
-         
+
       }
    }
 }
--- SyncServer/MainForm.cs
+++ SyncServer/MainForm.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.IO.Pipes;
-using System.Text;
 using System.Windows.Forms;
 
 namespace SyncServer
